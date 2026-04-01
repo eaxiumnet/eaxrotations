1d1d19f320fb5058d23b3c61aa7b866836dd6c3f
Eaxium <80918589+eaxiumnet@users.noreply.github.com>
2026-04-01 20:57:24 +0200

    fix: remove broken vendor_automation/mount_manager requires from 5 remaining specs
    - Hunter BM, Rogue Assassination, Druid Feral, Enh Shaman, Holy Paladin
    - These files still referenced deleted vendor_automation.lua and mount_manager.lua
    - Would crash at runtime without this fix

--- EAXPaladinHoly/main.lua
+++ EAXPaladinHoly/main.lua
@@ -21,12 +21,8 @@ local color     = require("libraries/color")
 local interrupt_manager = require("libraries/interrupt_manager")
 ---@type ooc_manager
 local ooc_manager = require("libraries/ooc_manager")
----@type vendor_automation
-local vendor_automation = require("libraries/vendor_automation")
 ---@type consumables_manager
 local consumables_manager = require("libraries/consumables_manager")
----@type mount_manager
-local mount_manager = require("libraries/mount_manager")
 ---@type leveling_manager
 local leveling_manager = require("libraries/leveling_manager")
 ---@type creature_utils
